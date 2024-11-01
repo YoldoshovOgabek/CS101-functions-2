0966b087da24e998cf5008075c88dc606cedab2f
o.yoldoshov <o.yoldoshov@newuu.uz>
2024-11-01 12:04:55 +0500

    HW added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
 //
 // Created by Og�Abek Yo�Ldoshov on 10/29/24.
 //
+#include <iostream>
+#include "problems.h"
+using namespace std;
+
+int main() {
+  cout << ceilFunction(5.5) << endl;
+  double a= 5, b=6;
+  swap_values(a,b);
+  cout << a << b << endl;
+  cout << multiply(a,b) << endl;
+  cout << digitSum(1234) << endl;
+  cout << decimalToBinary(5) << endl;
+  cout << midValue(15,17,14) << endl;
+}
--- problems.h
+++ problems.h
@@ -10,19 +10,60 @@ void exampleFunction() {
 
 // Problem 1
 //floorFunction
+int floorFunction(double num) {
+    return num/1;
+}
 //ceilingFunction
+int ceilFunction(double num) {
+    return (num+1)/1;
+}
 
 // Problem 2
 //swap_values
+void swap_values(double &num1, double &num2) {
+    num1 += num2;
+    num2 = num1 - num2;
+    num1 -= num2;
+}
 
 // Problem 3
 //multiply
-
+int multiply(int a, int b) {
+    if(b == 1) return a;
+    return a + multiply(a, b-1);
+}
 // Problem 4
 //digitSum
+int digitSum(int n) {
+    if(n == 0) return 0;
+    return n%10 + digitSum(n/10);
+}
 
 // Problem 5
 //decimalToBinary
+int decimalToBinary(int n, int place = 1) {
+    if (n == 0)
+        return 0;
+    return (n % 2) * place + decimalToBinary(n / 2, place * 10);
+}
 
 // Problem 6
 //midValue
+int midValue(int a, int b, int c) {
+    if ((a >= b && a <= c) || (a <= b && a >= c)) {
+        return a;
+    } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+        return b;
+    } else {
+        return c;
+    }
+}
+/*
+int - floorFunction
+int - ceilingFunction
+void - swap_values
+int - multiply
+int - digitSum
+int - decimalToBinary
+int - midValue
+ */
